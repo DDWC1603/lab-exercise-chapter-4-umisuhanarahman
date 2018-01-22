a6571912f3fadb0530afe86dffbcda4c47ef7f73
umisuhanarahman <33883955+umisuhanarahman@users.noreply.github.com>
2018-01-22 22:39:07 +0800

    task complete

---
+++ chpt4 27.cpp
@@ -0,0 +1,17 @@
+//umi suhana binti abdul rahman
+//A17DW2041
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n = 10;
+	
+	for (n=10;n<20;n++)
+    {
+	n!=2;
+    cout<<n*n<<endl;
+	}
+
+}
